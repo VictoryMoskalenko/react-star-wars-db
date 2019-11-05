b614b6822b8bba24dbfccca2bc946f6276ac701c
Victory Moskalenko <vita.moskalenko@gmail.com>
2019-11-05 15:20:17 +0200

    Some hftterns used

--- star-wars-db/src/components/people-page/people-page.js
+++ star-wars-db/src/components/people-page/people-page.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details/person-details';
 import ErrorIndicator from '../error-indicator/error-indicator';
-import SwapiService from "../../services/swapi-service";
+import SwapiService from '../../services/swapi-service';
+import Row from '../row'; 
 
 import './people-page.css';
+
 
 export default class PeoplePage extends Component {
 
@@ -33,16 +35,19 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />;
     }
 
+    const itemList = (
+      <ItemList onItemSelected={this.onPersonSelected}
+          getData={this.swapiService.getAllPeople}
+          renderItem={({name, gender, birthYear}) => (
+            `${name} (${gender}, ${birthYear})`)} />
+    );
+
+    const personDetails = (
+      <PersonDetails personId={this.state.selectedPerson} />
+    );
+
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList onItemSelected={this.onPersonSelected}
-          getData={this.swapiService.getAllPlanets} />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
-      </div>
+      <Row left={itemList} right={personDetails} />
     );
   }
 }
