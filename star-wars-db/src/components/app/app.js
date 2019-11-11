e06149a4ad6bd2532cdb425de111a20a409f45f9
Victory Moskalenko <vita.moskalenko@gmail.com>
2019-11-11 18:33:41 +0200

    routing added

--- star-wars-db/src/components/app/app.js
+++ star-wars-db/src/components/app/app.js
@@ -8,7 +8,7 @@ import DummySwapiService from '../../services/dummy-swapi-service';
 import ErrorBoundry from '../error-boundry/error-boundry';
 import { SwapiServiceProvider } from '../swapi-service-context/swapi-service-context';
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages'; 
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default class App extends Component {
    
@@ -33,19 +33,26 @@ export default class App extends Component {
             return (
               <ErrorBoundry>
                 <SwapiServiceProvider value={this.state.swapiService}>
-                  <div className="stardb-app">
+                  <Router>
+                    <div className="stardb-app">
 
-                    <Header onServiceChange={this.onServiceChange} />
+                      <Header onServiceChange={this.onServiceChange} />
 
-                    <RandomPlanet/>
+                      <RandomPlanet/>
 
-                    <PeoplePage />
+                      <Route path="/"
+                       render={() => <h2>Welcome to Star Wars DB</h2>}
+                       exact />
+                       <Route path="/people"
+                       render={() => <h2>People</h2>}
+                       exact />
 
-                    <PlanetsPage />
+                      <Route path="/people" component={PeoplePage} />
+                      <Route path="/planets" component={PlanetsPage} />
+                      <Route path="/starships" component={StarshipsPage} />
 
-                    <StarshipsPage />
-
-                  </div>
+                    </div>
+                  </Router>
                 </SwapiServiceProvider>
               </ErrorBoundry>
             );
--- star-wars-db/src/components/header/header.js
+++ star-wars-db/src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 import './header.css';
 
@@ -6,20 +7,22 @@ const Header = ({ onServiceChange }) => {
     return (
         <div className="header d-flex">
             <h3>
-                <a href='#/'>
-                    StarWars DB
-                </a>
+                <Link to="/">StarWars DB</Link>
+                
             </h3>
             
             <ul className="d-flex">
                 <li>
-                    <a href="#/people">People</a>
+                    <Link to="/people">People</Link>
+                    
                 </li>
                 <li>
-                    <a href="#/planets">Planets</a>
+                    <Link to="/planets">Planets</Link>
+                    
                 </li>
                 <li>
-                    <a href="#/starships">Starships</a>
+                    <Link to="/starships">Starships</Link>
+                   
                 </li>
             </ul>
 
