cc38f2487a9ba43874d2fe6c86594bdf0868bb8d
Victory Moskalenko <vita.moskalenko@gmail.com>
2019-11-15 16:26:37 +0200

    new app created

---
+++ re-store/src/components/app/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { HomePage, CardPage } from '../pages';
+import './app.css';
+
+const App = () => {
+    return (
+        <Switch>
+           <Route 
+              path="/"
+              component={HomePage}
+              exact
+            /> 
+            <Route
+              path="/card"
+              component={CardPage}
+             />
+        </Switch>
+    )
+};
+
+export default (App);
